4e61cf44a2ea5d0ddd70053e21d3f9a148693a47
Divarshecan <mahdibizixer70@gmail.com>
2026-08-27 15:42:26 +0330

    update site url

---
+++ assets/orders-store-Dz7-BQIQ.js
@@ -0,0 +1,1 @@
+import{a9 as t,aa as m}from"./index-YCoOcBKv.js";const n=t()(m((d,a)=>({orders:[],addOrder:r=>d(e=>({orders:[r,...e.orders]})),updateOrder:(r,e)=>d(o=>({orders:o.orders.map(s=>s.id===r?{...s,...e}:s)})),getByNumber:r=>a().orders.find(e=>e.number===r)}),{name:"asha-orders-v1",version:1}));export{n as u};
